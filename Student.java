97642d4aa4d87dc45eac5e96a7e5c0cef027b15b
blagicadimovska <dimovskablagica@yahoo.com>
2020-04-11 01:08:05 +0200

    student  class  finished

--- Student.java
+++ Student.java
@@ -2,12 +2,33 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-        SLL<Integer> labPoints;
-        
+	SLL<Integer> labPoints;
+
 
 	//TODO constructor
-
+     Student(String index, String firstName, String lastName){
+     	this.index=index;
+     	this.firstName=firstName;
+     	this.lastName=lastName;
+	 }
 	//TODO seters & getters
+	public static void setIndex(String newIndex){
+		indeks=newIndex;
+	}
+	public static void setIndex(String newFirstName){
+		firstName=newFirstName;
+	}
+	public static void setIndex(String newLastName){
+		lastName=newLastName;
+	}
+	public String getFirstName(){
+     	return this.firstName;
+	}
+	public String getLastName(){
+		return this.lastName;
+	}
+	
+
 
 	public double getAverage() {
 		//TODO
